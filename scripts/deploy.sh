a40f1a698e1e547db3e1721d8231c86f133889f7
nickdibari <ndibari@fordham.edu>
2020-06-02 23:37:16 -0400

    Add check for missing vault password file

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,8 +5,13 @@
 INVENTORY="inventory/prod"
 VAULT_PASSWORD_FILE=".vault_password"
 
+if [ ! -f "$VAULT_PASSWORD_FILE" ]; then
+	echo "ERROR: Missing vault password file"
+	exit 1
+fi
+
 ansible-playbook \
--i $INVENTORY \
+-i "$INVENTORY" \
 --ask-become-pass \
---vault-password-file=$VAULT_PASSWORD_FILE \
+--vault-password-file="$VAULT_PASSWORD_FILE" \
 site.yml
