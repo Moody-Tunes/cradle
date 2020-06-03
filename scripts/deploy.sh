1b2bbb33fe659a5a094459b594a9a58452b18d2d
nickdibari <ndibari@fordham.edu>
2020-06-02 23:20:44 -0400

    Add script for deploying moodytunes application

---
+++ scripts/deploy.sh
@@ -0,0 +1,10 @@
+# ansible command to deploy production application
+
+INVENTORY="inventory/prod"
+VAULT_PASSWORD_FILE=".vault_password"
+
+ansible-playbook \
+-i $INVENTORY \
+--ask-become-pass \
+--vault-password-file=$VAULT_PASSWORD_FILE \
+site.yml
