bb175ae3787a2183348405baea27a402139c6d63
nickdibari <ndibari@fordham.edu>
2020-10-17 22:22:19 -0400

    Refactor deploy script to specify provisioning args

--- scripts/deploy.bash
+++ scripts/deploy.bash
@@ -1,8 +1,30 @@
 #!/bin/bash
+set -euo pipefail
 
-# ansible command to deploy production application
+# Run ansible against a specified host
+# By default will provision the moodytunes host
 
-INVENTORY="inventory/prod_mtdj"
+usage () {
+	echo <<HELP "usage: bash scripts/deploy.bash [inventory] [playbook]
+
+Run ansible against a specified host. By default will provision the moodytunes host.
+
+inventory: Path to inventory file used in ansible run
+playbook: Playbook file to run with ansible"
+HELP
+}
+
+for arg in "$@"
+do
+    if [ "$arg" == "--help" ] || [ "$arg" == "-h" ]
+    then
+        usage
+        exit 0
+    fi
+done
+
+INVENTORY=${1:-"inventory/prod_mtdj"}
+PLAYBOOK=${2:-"mtdj.yml"}
 VAULT_PASSWORD_FILE=".vault_password"
 
 if [ ! "$(command -v ansible-playbook)" ]; then
@@ -15,8 +37,18 @@ if [ ! -f "$VAULT_PASSWORD_FILE" ]; then
 	exit 1
 fi
 
+if [ ! -f "$INVENTORY" ]; then
+	echo "ERROR: Could not find inventory file $INVENTORY"
+	exit 1
+fi
+
+if [ ! -f "$PLAYBOOK" ]; then
+	echo "ERROR: Could not find playbook file $PLAYBOOK"
+	exit 1
+fi
+
 ansible-playbook \
 -i "$INVENTORY" \
 --ask-become-pass \
 --vault-password-file="$VAULT_PASSWORD_FILE" \
-mtdj.yml
+"$PLAYBOOK"
