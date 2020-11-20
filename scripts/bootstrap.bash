051b00f021e68da9fc44f66916decb82a75a5bed
nickdibari <ndibari@fordham.edu>
2020-11-20 02:20:07 -0500

    First pass at adding script to bootstrap new hosts

---
+++ scripts/bootstrap.bash
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -euo pipefail
+
+host_ip="$1"
+
+
+### Create provisioning user
+provisioning_user="ansible"
+
+echo "Creating ansible user..."
+ssh root@$host_ip 'sudo grep $provisioning_user /etc/passwd > /dev/null 2>&1 || sudo useradd $provisioning_user'
+ssh root@$host_ip 'sudo usermod -aG sudo $provisioning_user'
