fdfc951f88ec313c441d9865f4bb9f036dd4391b
nickdibari <ndibari@fordham.edu>
2020-11-29 01:12:17 -0500

    Swap command for adding provisioning user

--- scripts/bootstrap.bash
+++ scripts/bootstrap.bash
@@ -8,5 +8,5 @@ host_ip="$1"
 provisioning_user="ansible"
 
 echo "Creating ansible user..."
-ssh root@$host_ip 'sudo grep $provisioning_user /etc/passwd > /dev/null 2>&1 || sudo useradd $provisioning_user'
+ssh root@$host_ip 'sudo grep $provisioning_user /etc/passwd > /dev/null 2>&1 || sudo adduser $provisioning_user'
 ssh root@$host_ip 'sudo usermod -aG sudo $provisioning_user'
